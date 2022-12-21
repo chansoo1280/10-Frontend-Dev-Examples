d106563c2cf2e574a295a38d96cddb89c712f5f6
chansoo1280 <chansoo1280@naver.com>
2022-12-21 21:46:21 +0900

    Feat Default Layout
    Default Layout 추가

--- blog/web-frontend/community/app/src/Components/Layouts/DefaultLayout/index.tsx
+++ blog/web-frontend/community/app/src/Components/Layouts/DefaultLayout/index.tsx
@@ -1,17 +1,16 @@
 // #region Global Imports
 import Head from "next/head"
+import { forwardRef } from "react"
+import classNames from "classnames"
 // #endregion Global Imports
 
 // #region Local Imports
 import styles from "./DefaultLayout.module.scss"
-import classNames from "classnames"
-import { forwardRef } from "react"
 import { LayoutProps } from ".."
 import { SiteHeader } from "@Components"
 // #endregion Local Imports
 
 const DefaultLayout = ({ children }: LayoutProps, ref: React.ForwardedRef<HTMLDivElement>): JSX.Element => {
-    const prefixCls = "wrap"
     return (
         <>
             <Head>
@@ -19,7 +18,7 @@ const DefaultLayout = ({ children }: LayoutProps, ref: React.ForwardedRef<HTMLDi
                 <meta name="description" content="" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div ref={ref} id="wrap" className={classNames(prefixCls, styles["default-layout"])}>
+            <div ref={ref} id="wrap" className={classNames(styles["default-layout"])}>
                 <SiteHeader />
                 {children}
             </div>
