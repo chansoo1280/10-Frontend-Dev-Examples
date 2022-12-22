28f81aa64f3a261b9b9eccb8b93ffe2bd08709b0
chansoo1280 <chansoo1280@naver.com>
2022-12-22 22:18:54 +0900

    feat component Typography
    1. as property 추가
    2. 스타일 추가

--- blog/web-frontend/community/app/src/Components/Atoms/Typography/index.tsx
+++ blog/web-frontend/community/app/src/Components/Atoms/Typography/index.tsx
@@ -1,11 +1,20 @@
-import { ReactNode } from "react"
+// #region Global Imports
+import React from "react"
+import classNames from "classnames"
+// #endregion Global Imports
 
-interface Props {
-    children: ReactNode
+// #region Local Imports
+import { defaultProps } from "@Components"
+import styles from "./Text.module.scss"
+// #endregion Local Imports
+
+interface TextProps extends defaultProps {
+    as?: "span" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
 }
 
-const Text = ({ children }: Props) => {
-    return <span>{children}</span>
+const Text = ({ as = "span", ...rest }: TextProps) => {
+    const TextNode = `${as}` as keyof JSX.IntrinsicElements
+    return <TextNode className={classNames(styles[`text`])} {...rest} />
 }
 const Typography = {
     Text,
