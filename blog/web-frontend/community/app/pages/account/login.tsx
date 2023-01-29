2dcb8524a33da41a3ddc766201e29381319532ad
chansoo1280 <chansoo1280@naver.com>
2023-01-29 13:06:04 +0900

    feat page
    1. login 상태 메세지 추가

--- blog/web-frontend/community/app/pages/account/login.tsx
+++ blog/web-frontend/community/app/pages/account/login.tsx
@@ -1,17 +1,18 @@
 // #region Global Imports
 import React, { createRef, FormEvent, useEffect, useState } from "react"
-import { useRouter } from "next/router"
+import { useQuery } from "react-query"
 // #endregion Global Imports
 
 // #region Local Imports
-import { Input, Space, Text, Checkbox, Button, Icon, AccountForm, Row } from "@Components"
+import { Space, Text, Checkbox, Button, Icon, AccountForm, Row, FormInput } from "@Components"
 import { Layout } from "@Components/Layouts"
 import { PageProps } from "../_app"
 import { useUser } from "@Hooks/useUser"
 import { HttpAccount } from "@Services/API/Account"
 import { useHistoryBack, usePrevPath } from "@Hooks/useHistoryBack"
-import { setStoredSessionId, useAccessToken } from "@Hooks/useAccessToken"
+import { setStoredSessionId } from "@Hooks/useAccessToken"
 import { ResMessageWithDesc, ResStatus } from "@Server/response"
+import { User } from "@Services/User"
 // #endregion Local Imports
 
 enum LoginState {
@@ -24,38 +25,32 @@ enum LoginState {
 }
 
 const Login = () => {
-    const router = useRouter()
-    const [loginState, setLoginState] = useState(LoginState.NONE)
-    const noticeRef = createRef<HTMLHeadingElement>()
-    const [keepLogin, setKeepLogin] = useState(false)
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-
-    const { updateUser } = useUser()
-
     const { historyBack } = useHistoryBack("/community/questionList")
     const { prevPath } = usePrevPath()
 
-    useEffect(() => {
-        console.log("useEffect")
-        if (loginState !== LoginState.NONE) {
-            noticeRef.current?.focus()
-        }
-    }, [loginState])
+    const [loginState, setLoginState] = useState(LoginState.NONE)
+    const stateNotice =
+        loginState === LoginState.NONE_USER
+            ? "회원정보가 존재하지 않습니다."
+            : loginState === LoginState.NONE_EMAIL
+            ? "이메일을 입력해주세요."
+            : loginState === LoginState.NONE_PW
+            ? "비밀번호를 입력해주세요."
+            : loginState === LoginState.ERROR
+            ? "이메일 or 비밀번호가 올바르지 않습니다."
+            : ""
+    const noticeRef = createRef<HTMLHeadingElement>()
 
-    const handleSubmitLogin = async (e: FormEvent) => {
-        e.preventDefault()
+    const [loginInfo, setLoginInfo] = useState<Pick<User, "email" | "password"> & { keepLogin: boolean }>({
+        email: "",
+        password: "",
+        keepLogin: false,
+    })
 
-        if (email === "") {
-            setLoginState(LoginState.NONE_EMAIL)
-            return
-        }
-        if (password === "") {
-            setLoginState(LoginState.NONE_PW)
-            return
-        }
+    const { updateUser } = useUser()
 
-        const result = await HttpAccount.login({ email, password, keepLogin }).catch((e: ResMessageWithDesc) => {
+    const login = async () =>
+        await HttpAccount.login(loginInfo).catch((e: ResMessageWithDesc) => {
             console.log(e)
             switch (e.status) {
                 case ResStatus.BadRequest:
@@ -70,43 +65,55 @@ const Login = () => {
             }
             return null
         })
-        if (result === null) {
+
+    const { isLoading, refetch } = useQuery("login", () => login(), {
+        enabled: false,
+    })
+
+    const handleSubmitLogin = async (e: FormEvent) => {
+        e.preventDefault()
+        if (loginInfo.email === "") {
+            setLoginState(LoginState.NONE_EMAIL)
             return
         }
-        if (result.sessionId !== null) {
-            setStoredSessionId(result.sessionId)
+        if (loginInfo.password === "") {
+            setLoginState(LoginState.NONE_PW)
+            return
         }
-        updateUser({ ...result, keepLogin })
+        const { data } = await refetch()
+        if (data === null || data === undefined) {
+            return
+        }
+        if (data.sessionId !== null) {
+            setStoredSessionId(data.sessionId)
+        }
+        updateUser({ ...data, keepLogin: loginInfo.keepLogin })
         historyBack()
         return
     }
+
+    useEffect(() => {
+        if (loginState !== LoginState.NONE) {
+            noticeRef.current?.focus()
+        }
+    }, [loginState])
     return (
         <>
             <AccountForm header="Login" onSubmit={handleSubmitLogin}>
                 <Text ref={noticeRef} tabIndex={0} widthType="wide" size="small" status="error">
-                    {loginState === LoginState.NONE_USER
-                        ? "회원정보가 존재하지 않습니다."
-                        : loginState === LoginState.NONE_EMAIL
-                        ? "이메일을 입력해주세요."
-                        : loginState === LoginState.NONE_PW
-                        ? "비밀번호를 입력해주세요."
-                        : loginState === LoginState.ERROR
-                        ? "이메일 or 비밀번호가 올바르지 않습니다."
-                        : ""}
+                    {stateNotice}
                 </Text>
                 <Row direction="vertical">
-                    <Input
+                    <FormInput
                         status={loginState === LoginState.NONE_USER || loginState === LoginState.NONE_EMAIL ? "error" : "normal"}
                         inputId="email"
                         label="email"
-                        widthType="wide"
-                        size="large"
                         prefix={<Icon iconName="xi-user-o" />}
                         placeholder="Email"
                         type="email"
-                        value={email}
+                        value={loginInfo.email}
                         onChange={(event) => {
-                            setEmail(event.target.value)
+                            setLoginInfo({ ...loginInfo, email: event.target.value })
                             if (loginState === LoginState.NONE_USER || loginState === LoginState.NONE_EMAIL) {
                                 setLoginState(LoginState.NONE)
                             }
@@ -114,18 +121,16 @@ const Login = () => {
                     />
                 </Row>
                 <Row direction="vertical" align="flex-end">
-                    <Input
+                    <FormInput
                         status={loginState === LoginState.NONE_PW ? "error" : "normal"}
                         inputId="password"
                         label="password"
-                        widthType="wide"
-                        size="large"
                         prefix={<Icon iconName="xi-lock-o" />}
                         placeholder="Password"
                         type="password"
-                        value={password}
+                        value={loginInfo.password}
                         onChange={(event) => {
-                            setPassword(event.target.value)
+                            setLoginInfo({ ...loginInfo, password: event.target.value })
                             if (loginState === LoginState.NONE_PW) {
                                 setLoginState(LoginState.NONE)
                             }
@@ -144,21 +149,17 @@ const Login = () => {
                         비밀번호를 잊으셨나요?
                     </Button>
                 </Row>
+                <Checkbox
+                    id="keepLogin"
+                    checked={loginInfo.keepLogin}
+                    onChange={() => {
+                        setLoginInfo({ ...loginInfo, keepLogin: !loginInfo.keepLogin })
+                    }}
+                    label="로그인 유지하기"
+                />
                 <Row>
                     <Space.Box>
-                        <Checkbox
-                            id="keepLogin"
-                            checked={keepLogin}
-                            onChange={() => {
-                                setKeepLogin(!keepLogin)
-                            }}
-                            label="로그인 유지하기"
-                        />
-                    </Space.Box>
-                </Row>
-                <Row>
-                    <Space.Box>
-                        <Button htmlType="submit" size="large">
+                        <Button loading={isLoading} htmlType="submit" size="large">
                             로그인
                         </Button>
                     </Space.Box>
