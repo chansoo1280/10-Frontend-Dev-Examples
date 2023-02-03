fbfa9d4e7c786d499fdff067444d13829df92049
chansoo1280 <chansoo1280@naver.com>
2023-02-03 22:00:01 +0900

    feat service
    1. 비밀번호 최대갯수 설정

--- blog/web-frontend/community/app/src/Services/User/User.entity.ts
+++ blog/web-frontend/community/app/src/Services/User/User.entity.ts
@@ -12,7 +12,7 @@ export interface User {
 }
 const requiredList: (keyof User)[] = ["email", "id", "name", "password", "salt", "created"]
 const emailRegexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-const passwordRegexp = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+const passwordRegexp = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/)
 const nameRegexp = new RegExp(/^[A-z0-9]{7,14}$/)
 const conditionOfKeys: ConditionOfKeys<User, keyof User> = {
     email: (target) => isString(target) && target.length <= 100 && emailRegexp.test(target),
